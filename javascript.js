06ba72a9eaa6e35e72a6ac57dbecd9050ccbd468
l-shaikh <shaikhlaraib115@gmail.com>
2025-01-13 10:18:06 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// const head1 = document.getElementById("demo")
+// const head2 = document.getElementById("t1")
+
+// head1.innerText="hello world"
+// head2.innerText="hello world"
+const btn=document.getElementById("btn")
+
+btn.addEventListener('click',()=>{
+    console.log("click on me")
+    alert("click me")
+})
